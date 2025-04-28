fcb3fae8542e586edaf168fbffe3794ca14fce7c
aansariaxioned <ansaria@axioned.com>
2025-04-28 18:01:09 +0530

    assignment completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import InfoCard from "@/components/ListCard";
+import ListCard from "@/components/ListCard";
 import Locationcard from "@/components/LocationCard";
 
-
-
-// import InfoCard from "@/components/InfoCard";
 const infoItems = [
   {
     title: "[Our team]",
@@ -19,19 +16,36 @@ const infoItems = [
   },
 ];
 
+const locationData = [
+  {
+    description: "[Optional location text] Nulla fringilla Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut saepe eum sint, excepturi fugiat minima ex debitis quo id!",
+    address: "208 Park Avenue",
+    city: "New York",
+    loaction: "NY 10017",
+    map: "Map",
+    telephone: "+1 212.364.7800"
+  },
+]
+
 export default function Home() {
   return (
-    <div className="@container space-y-16 p-2 sm:p-10 wrapper">
+    <div className="@container/main space-y-16 p-2 @sm:p-10 wrapper pt-10">
       <section>
         <h2 className="text-2xl font-bold mb-8">Info Card Grid</h2>
-        <InfoCard items={infoItems} layout="grid" className={`border-0 shadow-none`} />
+        <ListCard items={infoItems} layout="grid" className="border-0 shadow-none p-0" />
       </section>
       <section>
         <h2 className="text-2xl font-bold mb-8">Info List Section</h2>
-        <InfoCard items={infoItems} layout="list" className={`border-0 shadow-none gap-0 p-0` } />
+        <ListCard items={infoItems} layout="list" className="border-0 shadow-none p-0 gap-0" />
       </section>
 
-      <Locationcard />
+      <section>
+        <h2 className="text-2xl font-bold mb-8">Location Card</h2>
+        <h2 className="text-3xl font-semibold pb-5">New York</h2>
+
+
+        <Locationcard items={locationData} />
+      </section>
     </div>
   );
 }
--- components/LocationCard.tsx
+++ components/LocationCard.tsx
@@ -1,41 +1,56 @@
 import Image from "next/image"
 import image from "../public/png.png"
 
-export default function Locationcard() {
-  return (
-    <section id="location-card" className="py-10 grid justify-start">
-      <h1 className="text-2xl font-bold  py-10">Location Card</h1>
-      <h2 className="text-3xl font-semibold pb-5">New York</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+interface InfoCardProps {
+  items: Array<{
+    description: string;
+    address: string;
+    city: string;
+    loaction: string;
+    map: string;
+    telephone: string;
+  }>;
 
-      <div className="new-york-card  ">
-        <div className="image">
-          <Image
-            src={image}
-            width={600}
-            height={500}
-            alt="error"
-            className="" />
-        </div>
+}
 
-        <div className=" pt-5">
-          <p className="text-[15px] sm:text-xl text-gray-900">[Optional location text] Nulla fringilla Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut saepe eum sint, excepturi fugiat minima ex debitis quo id!</p>
-        </div>
+export default function Locationcard({ items=[] }: InfoCardProps) {
+    return (
+            <div className="@container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+
+              {items.map((item,index) => {
+                return(
+                  <div key={index} className="new-york-card  ">
+                  <div className="image">
+                      <Image
+                          src={image}
+                          width={600}
+                          height={500}
+                          alt="error"
+                          className=""/>
+                  </div>
+
+                  <div className=" pt-5">
+                      <p className="text-[15px] sm:text-xl text-gray-900">{item.description}</p>
+                  </div>
 
 
-        <div className=" text-[15px] sm:text-xl text-gray-900 py-8  ">
-          <p>208 Park Avenue</p>
-          <p>New York</p>
-          <p>NY 10017</p>
-        </div>
+                  <div className=" text-[15px] sm:text-xl text-gray-900 py-8  ">
+                      <p>{item.address}</p>
+                      <p>{item.city}</p>
+                      <p>{item.loaction}</p>
+                  </div>
 
-        <p className="text-[15px] sm:text-xl text-gray-900 pb-2 underline">Map</p>
+                  <p className="text-[15px] sm:text-xl text-gray-900 pb-2 underline">{item.map}</p>
 
-        <p><a href="tel:+1 212.364.7800">+1 212.364.7800</a></p>
-      </div>
+                  <p><a href="tel:+1 212.364.7800">{item.telephone}</a></p>
+              </div>
+                )
+              })}
+
+                
             </div>
 
-    </section>
+      
 
-  )
+    )
 }
